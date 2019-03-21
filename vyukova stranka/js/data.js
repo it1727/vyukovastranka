f424105af32b4a0dfd54601636e6811c235f10af
it1727 <36028800+it1727@users.noreply.github.com>
2019-03-21 20:45:32 +0100

    1.0

---
+++ vyukova stranka/js/data.js
@@ -0,0 +1,40 @@
+var osobnosti = [
+    {
+        "name":"Adolf Hitler",
+        "photo":"osobnost1.jpg",
+    },
+    {
+        "name":"Josif Vissarionovič Stalin",
+        "photo":"osobnost2.jpg",
+    },
+    {
+        "name":"Winston Churchill",
+        "photo":"osobnost3.jpg",
+    },
+    {
+        "name":"Franklin Delano Roosevelt",
+        "photo":"osobnost4.jpg",
+        "borned":"23. 11. 1896 Dědice",
+    },
+    {
+        "name":"Benito Mussolini",
+        "photo":"osobnost5.jpg",
+    },            
+    {
+        "name":"Georgij Konstantinovič Žukov",
+        "photo":"osobnost6.jpg",
+    },            
+    {
+        "name":"Erwin Rommel",
+        "photo":"osobnost7.jpg",
+    },            
+    {
+        "name":"Bernard Montgomery",
+        "photo":"osobnost8.jpg",
+    },
+    {
+        "name":"George S. Patton",
+        "photo":"osobnost9.jpg",
+    },                   
+];
+
---
+++ vyukova stranka/js/main.js
@@ -0,0 +1,45 @@
+$(function(){
+    $('#ustava p').hide();
+    $('#ustava h4').on('click', function(){
+        if ($(this).nextUntil('h4').is(':hidden')) {
+            $(this).css({'background-color':'#38a'});
+        } else {
+            $(this).css({'background-color':'#444'});
+        }
+        $(this).nextUntil('h4').toggle(500);
+    });
+
+    var kviz = $('#kviz div.row');
+    osobnosti.forEach(function(obj, idx){
+        console.log(obj.photo);
+        kviz.append('<div class="col-sm-4"><figure id="'+idx+'"><img src="img/osobnost0.jpg" alt="Osobnost"><figcaption>'
+        +obj.name+'</figcaption></figure></div>');
+    });
+
+    var photo = $('#kviz img');
+    photo.on('click',function(){
+        var index = Math.floor(Math.random()*osobnosti.length);
+        $(this).attr('src','img/'+osobnosti[index].photo)
+               .attr('alt',osobnosti[index].name);
+    });
+
+    var check = $('#kviz .btn-success');
+    check.on('click', function(){
+        $('#kviz figure').each(function(idx,obj){
+            var figcaption = $(obj).find('figcaption').text();
+            var alt = $(obj).find('img').attr('alt');
+            if (figcaption == alt) {
+                $(obj).find('img').css({'border':'2px solid green'}); 
+            } else {
+                $(obj).find('img').css({'border':'2px solid red'});
+            }
+        });        
+    });
+
+    var odkazy = $('#odkazy ul');
+    osobnosti.forEach(function(obj, idx){
+        odkazy.append('<li><a href="'+obj.link+'" target="_blank">'
+        +obj.name+'</a></li>');
+    });
+
+})
